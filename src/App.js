fbf33df8ab22dedd4ae8108b45aeb480d576c4eb
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-26 12:31:24 +0200

    setup Dashboard folder with own pages + componentes relative to each page, Routing and

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Landing, Error, Dashboard, Register } from './pages';
+import { Landing, Error, Register } from './pages';
+import {
+  Stats,
+  Profile,
+  AddJob,
+  AllJobs,
+  SharedLayout,
+} from './pages/Dashboard';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -8,7 +15,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="landing" element={<Landing />} />
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
---
+++ src/components/smallSidebar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const smallSidebar = () => {
+  return <div>smallSidebar</div>;
+};
+
+export default smallSidebar;
